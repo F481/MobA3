95dc091e858e315a7b5695b9b140a0cc6bfac4c0
F481 <fabi.kcirf@gmail.com>
2017-07-19 15:10:28 +0200

    added dummy routes for products interaction

---
+++ backend/routes/products.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET all products */
+router.get('/', function(req, res, next) {
+    //res.send('respond with a resource');
+    // TODO
+});
+
+router.post('/', function (req, res, next) {
+    // req.body includes params
+});
+
+router.put('/:id', function (req, res, next) {
+    // req.params.id
+    // TODO update product by id
+});
+
+router.delete('/:id', function (req, res, next) {
+    // TODO delete product by id
+});
+
+module.exports = router;
