6ca725b9a2c8610d3000cd7ca47ec3ae8cb088d6
F481 <fabi.kcirf@gmail.com>
2017-07-19 15:24:57 +0200

    formatted code

--- backend/app.js
+++ backend/app.js
@@ -20,16 +20,16 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to the db
-MongoClient.connect("mongodb://localhost:27017/moba3db", function(err, db) {
-  if (err) return console.log(err);
+MongoClient.connect("mongodb://localhost:27017/moba3db", function (err, db) {
+    if (err) return console.log(err);
 
-  console.log("connected to database");
-  
+    console.log("connected to database");
+
 });
 
 app.use('/', index);
@@ -37,21 +37,21 @@ app.use('/users', users);
 app.use('products', products);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
