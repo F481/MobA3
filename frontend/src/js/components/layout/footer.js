819689734e79b655fea74090d65dc70bf4fb69a8
Wolfburn <wolfgang.hermann@hs-weingarten.de>
2017-07-22 18:17:34 +0200

    initial commit
    - added frontend views
    - router / redux integration
    - reducers / providers (without actions)
    - trashed old ferdi branch skeleton (sorry ferdi)

---
+++ frontend/src/js/components/layout/footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <hr/>
+                        <p>Copyright &copy; WhyskyQueen</p>
+                        <a href="#">Über uns</a>
+                        <hr/>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
---
+++ frontend/src/js/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Whisky from "../components/whisky";
+
+export default class Archives extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Willkommen!</h1>
+      </div>
+    );
+  }
+}
