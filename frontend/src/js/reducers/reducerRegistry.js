c239078073e6c2682e670784280d8415b1cfcedd
ferdinand89 <ferdinand@koenigonline.eu>
2017-07-25 22:27:30 +0200

    Registry added success state in reducer

--- frontend/src/js/reducers/reducerRegistry.js
+++ frontend/src/js/reducers/reducerRegistry.js
@@ -6,15 +6,15 @@ export default function reducer(state = INITIAL_STATE, action) {
     let error;
     switch (action.type){
         case INIT_REGISTRY: {
-            return {...state, error: null, fetching: true, fetched: false};
+            return {...state, error: null, fetching: true, fetched: false, success: false};
         }
         case REGISTRY_SUCCESS: {
             console.log("Registry Success!!!")
-            return {...state, error: null, fetching: false, fetched: true};
+            return {...state, error: null, fetching: false, fetched: true, success: true};
         }
         case REGISTRY_ERROR: {
             error = action.payload || {message: action.payload.message};
-            return {...state, error: error, fetching: false, fetched: false};
+            return {...state, error: error, fetching: false, fetched: false, success: false};
         }
     }
     return state;
